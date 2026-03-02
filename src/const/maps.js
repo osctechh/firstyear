56672c2176e7b2b458902c6693fab74bb301efb5
Alexis Oscar <oscarcabj94@gmail.com>
2026-03-02 05:38:00 -0300

    agregado carrusel

---
+++ src/const/maps.js
@@ -0,0 +1,155 @@
+export const MAPS = [
+  {
+    featureType: "all",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#b263e5",
+      },
+    ],
+  },
+  {
+    featureType: "all",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        gamma: 0.01,
+      },
+      {
+        lightness: 20,
+      },
+    ],
+  },
+  {
+    featureType: "all",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        saturation: -31,
+      },
+      {
+        lightness: -33,
+      },
+      {
+        weight: 2,
+      },
+      {
+        gamma: 0.8,
+      },
+    ],
+  },
+  {
+    featureType: "all",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "landscape",
+    elementType: "geometry",
+    stylers: [
+      {
+        lightness: 30,
+      },
+      {
+        saturation: 30,
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [
+      {
+        saturation: 20,
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        lightness: "-100",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        lightness: "100",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        lightness: "-100",
+      },
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        lightness: 20,
+      },
+      {
+        saturation: -20,
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        lightness: 10,
+      },
+      {
+        saturation: -30,
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        saturation: 25,
+      },
+      {
+        lightness: 25,
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "all",
+    stylers: [
+      {
+        lightness: -20,
+      },
+    ],
+  },
+];
